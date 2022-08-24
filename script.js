db1460b3feb68835e61bda18a6edf1ae5a54b616
spangen87 <rikard.spangmyr@bestlas.se>
2022-08-24 09:14:26 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
     $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
+          $(".stream1").fadeOut();
+          $(".stream1").toggle("slow");
+          $(".stream2").toggle("medium");
+          $(".stream3").toggle("fast");
+          $(".stream3").toggle(1000);
    });
    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
@@ -17,4 +18,7 @@ $(document).ready(function() {
         $(".stream3").removeClass('highlight_stream');
         $(".stream3").addClass('highlight_stream');
    });
+   $("#myButton").removeClass("blueBox").addClass("border");
+   $("p").css("color", "blue").slideUp(2000).slideDown(2000);
+   $("a").attr("href", "http://www.example.com")
 }); 
